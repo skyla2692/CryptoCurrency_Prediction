427aa0a9353a1a1a36b81050b7e9f0ffa76ef6df
skylalee <reloxo99@gmail.com>
2020-11-25 02:36:39 +0900

    LSTM (20.11.25.ver.)

--- check_data.py
+++ check_data.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import LSTM, Dropout, Dense, Activation
 from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau
-from numpy import savetxt
 
 df = pd.read_csv('final_data.csv')
 
@@ -17,14 +16,10 @@ label = df['label'].values
 
 seq_len = 70  # window size 지정
 sequence_length = seq_len + 1
-
-mid_price_result = []
+result = []
 for index in range(len(mid_prices) - sequence_length):
-    mid_price_result.append(mid_prices[index: index + sequence_length])
+    result.append(mid_prices[index: index + sequence_length])
 
-label_result = []
-for index in range(len(mid_prices) - sequence_length):
-    label_result.append(mid_prices[index: index + sequence_length])
 
 def normalize_windows(data):
     normalized_data = []
@@ -33,16 +28,9 @@ def normalize_windows(data):
         normalized_data.append(normalized_window)
     return np.array(normalized_data)
 
-mid_price_result = normalize_windows(mid_price_result)
-label_result = normalize_windows(label_result)
 
-result = np.c_[mid_price_result, label_result]
-
-#print(result.shape)
-#savetxt('result_data.csv', result, delimiter=',')
-
-result = pd.read_csv('result_data.csv')
-result = result.values
+result = normalize_windows(result)
+print(result.shape)
 
 # split train and test data
 row = int(len(result) * 0.67)
@@ -50,11 +38,11 @@ train = result[:row, :]
 np.random.shuffle(train)
 
 x_train = train[:, :-1]
-x_train = np.reshape(x_train, (x_train.shape[0], 1, x_train.shape[1]))
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 y_train = train[:, -1]
 
 x_test = result[row:, :-1]
-x_test = np.reshape(x_test, (x_test.shape[0], 1, x_test.shape[1]))
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 y_test = result[row:, -1]
 
 print(x_train.shape, x_test.shape)
@@ -63,9 +51,9 @@ checkpoint_path = "training1.h5"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 model = Sequential()
-model.add(LSTM(256, return_sequences=True, input_shape=(x_train.shape[1], x_train.shape[2])))   # seq_len = 70
+model.add(LSTM(256, return_sequences=True, input_shape=(seq_len, 2)))   # seq_len = 70
 model.add(keras.layers.Dropout(0.2))
-model.add(LSTM(128, return_sequences=True))
+model.add(LSTM(128, return_sequences=False))
 model.add(keras.layers.Dropout(0.2))
 model.add(Dense(1, activation='linear'))
 model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
@@ -90,8 +78,7 @@ model.fit(x_train, y_train,
           ])
 
 pred = model.predict(x_test)
-print(pred.shape)
-'''
+
 fig = plt.figure(facecolor='white', figsize=(20, 10))
 ax = fig.add_subplot(111)
 ax.plot(y_test, label='True')
@@ -106,4 +93,3 @@ model.load_weights("training1.h5")
 # 모델을 재평가합니다
 loss, acc = model.evaluate(x_train, y_train, verbose=2)
 print("복원된 모델의 정확도: {:5.2f}%".format(100 * acc))
-'''
